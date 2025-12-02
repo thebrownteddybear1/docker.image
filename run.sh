4b8f56c64dba9f7d06f1fd73f6f4077bc5d7d884
thebrownteddybear1 <thebrownteddybear@gmail.com>
2025-12-02 18:24:26 +0000

    first

---
+++ run.sh
@@ -0,0 +1,5 @@
+docker compose up -d
+
+dockcer exec -it ansible /bin/bash
+
+#docker compose down
---
+++ start.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+/usr/sbin/sshd -D
+
+
+# Set your username and email
+git config --global user.name "teddybear"
+git config --global user.email "thebrownteddybear@gmail.com"
+
+# Optional: Set default editor
+git config --global core.editor "vim"
+
+# Check your configuration
+git config --list
+
+# Navigate to your project folder
+cd /root
+
+# Initialize git repository
+git init
+
+# Add all files to staging
+git add .
+
+# Commit files
+git commit -m "Initial commit"
+
+GITHUB_TOKEN=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
+git clone https://github.com/thebrownteddybear1/tonjiak.git
+
+
+#keep the container running.. tail follow  null forever
+tail -f /dev/null
+
+#!/bin/bash
+/usr/sbin/sshd -D
+
+
+# Set your username and email
+git config --global user.name "teddybear"
+git config --global user.email "thebrownteddybear@gmail.com"
+
+# Optional: Set default editor
+git config --global core.editor "vim"
+
+# Check your configuration
+git config --list
+
+# Navigate to your project folder
+cd /root
+
+# Initialize git repository
+git init
+
+# Add all files to staging
+git add .
+
+# Commit files
+git commit -m "Initial commit"
+
+GITHUB_TOKEN=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
+git clone https://github.com/thebrownteddybear1/tonjiak.git
+
+
+#keep the container running.. tail follow  null forever
+tail -f /dev/null
+
