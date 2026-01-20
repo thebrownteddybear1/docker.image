9741f5c666154eed547d540a1a4b5f0b8009c372
thebrownteddybear <thebrownteddybear@gmail.com>
2026-01-20 16:28:12 +0000

    asdf

--- how.nfs.readme.josephyap.sh
+++ how.nfs.readme.josephyap.sh
@@ -4,6 +4,9 @@ set -x
 sudo apt update
 sudo apt -y install nfs-kernel-server
 chown nobody:nogroup /mnt/nfs1
-echo "0.0.0.0 (rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/mnt/nfs1 0.0.0.0 (rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 systemctl restart nfs-kernel-server
 systemctl disable ufw
+systemctl stop ufw
+systemctl status ufw
+
