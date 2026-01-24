b0d1e42f2e676590f9b770d507a67ee994198c72
thebrownteddybear1 <thebrownteddybear@gmail.com>
2026-01-24 12:55:47 +0800

    asfd

--- frr/startup.sh
+++ frr/startup.sh
@@ -14,7 +14,7 @@ ip link add link ens33 name ens33.11 type vlan id 11
 ip link set ens33.11 up
 ip addr add 10.11.11.53/24 dev ens33.11
 
-# Create VLAN interface on ens33 51
+# Create VLAN interface on ens33 51 vmotion
 ip link add link ens33 name ens33.51 type vlan id 51
 ip link set ens33.51 up
 ip addr add 192.168.51.53/24 dev ens33.51
@@ -29,6 +29,13 @@ ip addr add 192.168.52.53/24 dev ens33.52
 ip link add link ens33 name ens33.53 type vlan id 53
 ip link set ens33.53 up
 ip addr add 192.168.53.53/24 dev ens33.53
+
+
+# Create VLAN interface on ens33 54 NFS
+ip link add link ens33 name ens33.54 type vlan id 54
+ip link set ens33.54 up
+ip addr add 192.168.54.53/24 dev ens33.54
+
 
 # Enable IP forwarding
 sysctl -w net.ipv4.conf.all.forwarding=1
