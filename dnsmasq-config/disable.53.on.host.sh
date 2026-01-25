90002d8fc4c02a31c178c8d80ed714c74fd2c435
thebrownteddybear@gmail.com <thebrownteddybear@gmail.com>
2026-01-25 07:02:57 +0000

    asdf

---
+++ dnsmasq-config/disable.53.on.host.sh
@@ -0,0 +1,6 @@
+# One-liner to disable systemd-resolved's DNS stub
+sudo sed -i '/^\[Resolve\]/,/^\[/ s/^#\?DNSStubListener=.*/DNSStubListener=no/' /etc/systemd/resolved.conf
+sudo sed -i '/^\[Resolve\]/,/^\[/ s/^#\?DNS=.*/DNS=127.0.0.1/' /etc/systemd/resolved.conf
+echo -e "nameserver 127.0.0.1\noptions edns0 trust-ad" | sudo tee /etc/resolv.conf
+sudo systemctl restart systemd-resolved
+sudo ss -tulpn | grep :53
