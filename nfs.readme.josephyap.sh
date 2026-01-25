4b2d735202a595a1ef32a90449e87b0461510fcf
thebrownteddybear1 <thebrownteddybear@gmail.com>
2026-01-26 04:38:51 +0800

    asf

--- nfs.readme.josephyap.sh
+++ nfs.readme.josephyap.sh
@@ -11,3 +11,12 @@ systemctl stop ufw
 systemctl status ufw
 
 #SET BOTH THE PARENT ENSI AND NFS SERVER TO TRUNK!!!
+
+#after you change something restart nfs
+# For NFSv3/NFSv4
+systemctl restart nfs-server
+# or
+systemctl restart nfs-kernel-server
+
+# Sometimes also need:
+systemctl restart rpcbind
