0f4d7c5a4a312b869dc42dbb4e539a96818cb3f7
Your Name <you@example.com>
2026-02-01 22:34:38 +0000

    asdf

--- offlinedepot/vpn/vpn.down.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -x
-ps aux | grep -v -e defunct -e grep | grep openconnect | awk '{print $2}' | xargs kill -9
-
--- offlinedepot/vpn/vpn.up.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -x
-openconnect -u 'ty036880@broadcom.net'      --protocol=gp --csd-wrapper /usr/libexec/openconnect/hipreport.sh      portal.vpn.broadcom.com -b
-
