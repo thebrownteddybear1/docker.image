a5b04304fef80a411ed1537829790c1599446e20
thebrownteddybear1 <thebrownteddybear@gmail.com>
2026-02-08 08:24:41 +0800

    asdf

--- nfs/install.nfs.sh
+++ nfs/install.nfs.sh
@@ -6,6 +6,11 @@ sudo apt update
 sudo apt -y install nfs-kernel-server
 chown nobody:nogroup /mnt/nfs1
 echo "/mnt/nfs1 *(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/mnt/nfs2 *(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/mnt/nfs3 *(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+
+exportfs -ra
+
 systemctl restart nfs-kernel-server
 systemctl disable ufw
 systemctl stop ufw
